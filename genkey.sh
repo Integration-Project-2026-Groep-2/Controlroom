af1975a0762f840f43c5a212b3205262adeb2e00
nasr <nsrddyn@gmail.com>
2026-03-31 17:45:20 +0200

    refactor(main): no more new line issue

--- genkey.sh
+++ genkey.sh
@@ -1,9 +1,14 @@
- #!/bin/sh
+#!/bin/sh
 
 KEY=$(openssl rand -hex 16)
 
 if [ -f .env ]; then
-    echo "xpack.encryptedSavedObjects.encryptionKey=$KEY" >> .env
+    if grep -q "^KIBANA_ENCRYPTION_KEY=" .env; then
+        # probeer die lijn weg te doen met kibana en dan gewoon vervangen snapje colle dingen
+        sed -i "s/^KIBANA_ENCRYPTION_KEY=.*/&$KEY/" .env
+    else
+        echo "KIBANA_ENCRYPTION_KEY=$KEY" >> .env
+    fi
 else
-    echo "xpack.encryptedSavedObjects.encryptionKey=$KEY" > .key
+    echo "KIBANA_ENCRYPTION_KEY=$KEY" > .env
 fi
