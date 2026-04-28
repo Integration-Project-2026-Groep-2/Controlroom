7a8a812fee966220455cfd174cdb269eb24ec1af
nasr <nsrddyn@gmail.com>
2026-04-28 21:48:14 +0200

    refactor(logger): removed OOP style from logger
    - no more global object.
    - message struct
    - cleaned up indexing

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -54,42 +54,54 @@ var journalPriorities = map[Severity]journal.Priority{
 	PANIC: journal.PriCrit,
 }
 
+func NewMessage(sev Severity, svc Service, data string) Message {
+	return Message{severity: sev, service: svc, data: data}
+}
+
 func Init(addr, idx string, writer io.Writer, workers int) error {
 	c, err := elasticsearch.NewClient(elasticsearch.Config{Addresses: []string{addr}})
 	if err != nil {
 		return err
 	}
+
 	esClient, esIndex, out, queue = c, idx, writer, make(chan []byte, 512)
+
 	for range workers {
-		go indexToElastic()
+		go indexQueue()
 	}
 	return nil
 }
 
 func Shutdown() { close(queue) }
 
-func Log(severity Severity, svc Service, msg string) {
+type Message struct {
+	severity Severity
+	service  Service
+	data     string
+}
+
+func Log(msg Message) {
 	buf := pool.Get().(*bytes.Buffer)
 	buf.Reset()
 
 	var tmp [35]byte
 	buf.WriteString(`{"level":"`)
-	buf.WriteString(string(severity))
+	buf.WriteString(string(msg.severity))
 	buf.WriteString(`","@timestamp":"`)
 	buf.Write(time.Now().UTC().AppendFormat(tmp[:0], time.RFC3339Nano))
 	buf.WriteString(`","service":"`)
-	writeEscaped(buf, string(svc))
+	writeEscaped(buf, string(msg.service))
 	buf.WriteString(`","msg":"`)
-	writeEscaped(buf, msg)
+	writeEscaped(buf, string(msg.data))
 	buf.WriteString("\"}\n")
 
 	out.Write(buf.Bytes())
 
-	switch severity {
+	switch msg.severity {
 	case ERROR, FATAL, PANIC:
-		journal.Send(msg, journalPriorities[severity], map[string]string{
-			"SERVICE":  string(svc),
-			"SEVERITY": string(severity),
+		journal.Send(msg.data, journalPriorities[msg.severity], map[string]string{
+			"SERVICE":  string(msg.service),
+			"SEVERITY": string(msg.severity),
 		})
 	}
 
@@ -108,12 +120,12 @@ func Log(severity Severity, svc Service, msg string) {
 	pool.Put(buf)
 
 	// an external service shouldn't be able to crash controlroom on command
-	if svc == CONTROLROOM && (severity == FATAL || severity == PANIC) {
+	if msg.service == CONTROLROOM && (msg.severity == FATAL || msg.severity == PANIC) {
 		os.Exit(1)
 	}
 }
 
-func indexToElastic() {
+func indexQueue() {
 	for payload := range queue {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		res, err := (esapi.IndexRequest{
@@ -133,16 +145,22 @@ func writeEscaped(buf *bytes.Buffer, s string) {
 	for i := 0; i < len(s); i++ {
 		c := s[i]
 		switch {
-			case c == '"':  buf.WriteString(`\"`)
-			case c == '\\': buf.WriteString(`\\`)
-			case c == '\n': buf.WriteString(`\n`)
-			case c == '\r': buf.WriteString(`\r`)
-			case c == '\t': buf.WriteString(`\t`)
+		case c == '"':
+			buf.WriteString(`\"`)
+		case c == '\\':
+			buf.WriteString(`\\`)
+		case c == '\n':
+			buf.WriteString(`\n`)
+		case c == '\r':
+			buf.WriteString(`\r`)
+		case c == '\t':
+			buf.WriteString(`\t`)
 		case c < 0x20:
 			buf.WriteString(`\u00`)
 			buf.WriteByte(hex[c>>4])
 			buf.WriteByte(hex[c&0xF])
-			default: buf.WriteByte(c)
+		default:
+			buf.WriteByte(c)
 		}
 	}
 }
