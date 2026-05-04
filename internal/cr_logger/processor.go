abca81e9694725d634d8b47dc825f862406e4d05
nasr <nsrddyn@gmail.com>
2026-05-04 17:35:04 +0200

    bugfix(logger): unmarshaling into json, confused with indexing

--- internal/cr_logger/processor.go
+++ internal/cr_logger/processor.go
@@ -1,17 +1,16 @@
 package cr_logger
 
 import (
-	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"github.com/elastic/go-elasticsearch/v9"
 	"integration-project-ehb/controlroom/pkg/gen"
 	"integration-project-ehb/controlroom/pkg/logger"
 )
 
-// ProcessLog unmarshals a LogEvent from RabbitMQ and indexes it
 func ProcessLog(_ *elasticsearch.Client, body []byte) error {
 	var event gen.LogEvent
-	err := json.Unmarshal(body, &event)
+	err := xml.Unmarshal(body, &event)
 	if err != nil {
 		logger.Log(logger.NewMessage(logger.ERROR, logger.CONTROLROOM, fmt.Sprintf("unparseable log body: %s", body)))
 		return fmt.Errorf("Failed to process Log: %s", err.Error())
