2bd32cb642299ec0b9011cdd95e326131134e328
nasr <nsrddyn@gmail.com>
2026-05-04 17:24:40 +0200

    bugfix(logger): json unmarshal was returning an empty body becuse we were parsing into the wrong struct.

--- internal/cr_logger/processor.go
+++ internal/cr_logger/processor.go
@@ -3,30 +3,21 @@ package cr_logger
 import (
 	"encoding/json"
 	"fmt"
-
 	"github.com/elastic/go-elasticsearch/v9"
+	"integration-project-ehb/controlroom/pkg/gen"
 	"integration-project-ehb/controlroom/pkg/logger"
 )
 
-type rawLogBody struct {
-	Level   string `json:"level"`
-	Service string `json:"service"`
-	Msg     string `json:"msg"`
-}
-
+// ProcessLog unmarshals a LogEvent from RabbitMQ and indexes it
 func ProcessLog(_ *elasticsearch.Client, body []byte) error {
-
-	var raw rawLogBody
-
-	err := json.Unmarshal(body, &raw)
-
-	fmt.Println("DEBUGGING: ", raw)
+	var event gen.LogEvent
+	err := json.Unmarshal(body, &event)
 	if err != nil {
 		logger.Log(logger.NewMessage(logger.ERROR, logger.CONTROLROOM, fmt.Sprintf("unparseable log body: %s", body)))
 		return fmt.Errorf("Failed to process Log: %s", err.Error())
 	}
 
-	logger.Log(logger.NewMessage(logger.Severity(raw.Level), logger.Service(raw.Service), raw.Msg))
-
+	// Map gen.SeverityType to logger.Severity
+	logger.Log(logger.NewMessage(logger.Severity(event.Level), logger.Service(event.Service), event.Data))
 	return nil
 }
