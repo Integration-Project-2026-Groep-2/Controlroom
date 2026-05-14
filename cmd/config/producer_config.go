911f6e0777b467152559400dc0684ac08bd124b8
nasr <nsrddyn@gmail.com>
2026-05-14 23:09:07 +0200

    feature(publishers): configuration for watchdogevents for the mcp server

--- cmd/config/producer_config.go
+++ cmd/config/producer_config.go
@@ -1,58 +1,69 @@
 package config
 
 import (
-    "integration-project-ehb/controlroom/internal/cr_rabbitmq"
+	"integration-project-ehb/controlroom/internal/cr_rabbitmq"
 )
 
 type cr_producer_t int8
 type cr_producer_events_t int8
 
 const (
-    HEARTBEAT_FAILED_EVENT = iota
-    HEARTBEAT_SUCCEEDED_EVENT
-    WARNING_EVENT
+	HEARTBEAT_FAILED_EVENT = iota
+	HEARTBEAT_SUCCEEDED_EVENT
+	WARNING_EVENT
+	RMQ_HEARTBEAT
 )
 
 const (
-    WATCHDOG cr_producer_t = iota
+	WATCHDOG cr_producer_t = iota
 	CONTROLROOM
+	RMQ
 )
 
 type ProducerDef struct {
-    Type     cr_producer_t
-    Exchange cr_rabbitmq.ExchangeInfo
-    Key      cr_rabbitmq.BindingInfo
-    Queue    cr_rabbitmq.QueueInfo
+	Type     cr_producer_t
+	Exchange cr_rabbitmq.ExchangeInfo
+	Key      cr_rabbitmq.BindingInfo
+	Queue    cr_rabbitmq.QueueInfo
 }
 
 var Producer = map[cr_producer_events_t]ProducerDef{
-    HEARTBEAT_FAILED_EVENT: {
-        Type: WATCHDOG,
-        Exchange: cr_rabbitmq.ExchangeInfo{
-            Name:    "ai.events",
-            Kind:    "topic",
-            Durable: true,
-        },
-        Key: cr_rabbitmq.BindingInfo{
-            Key: "event.heartbeat_failed",
-        },
-    },
-    HEARTBEAT_SUCCEEDED_EVENT: {
-        Type: WATCHDOG,
-        Exchange: cr_rabbitmq.ExchangeInfo{
-            Name:    "ai.events",
-            Kind:    "topic",
-            Durable: true,
-        },
-        Key: cr_rabbitmq.BindingInfo{
-            Key: "event.heartbeat_succeeded",
-        },
-    },
+
+	HEARTBEAT_FAILED_EVENT: {
+		Type: WATCHDOG,
+		Exchange: cr_rabbitmq.ExchangeInfo{
+			Name:    "ai.events",
+			Kind:    "topic",
+			Durable: true,
+		},
+		Key: cr_rabbitmq.BindingInfo{
+			Key: "event.heartbeat_failed",
+		},
+	},
+
+	HEARTBEAT_SUCCEEDED_EVENT: {
+		Type: WATCHDOG,
+		Exchange: cr_rabbitmq.ExchangeInfo{
+			Name:    "ai.events",
+			Kind:    "topic",
+			Durable: true,
+		},
+		Key: cr_rabbitmq.BindingInfo{
+			Key: "event.heartbeat_succeeded",
+		},
+	},
 
 	WARNING_EVENT: {
 		Type:     0,
 		Exchange: cr_rabbitmq.ExchangeInfo{Name: "news.topic", Kind: "topic"},
 		Key:      cr_rabbitmq.BindingInfo{Key: "news.warning"},
-		Queue: cr_rabbitmq.QueueInfo{Name: "mailing.news.warning", Durable: true},
+		Queue:    cr_rabbitmq.QueueInfo{Name: "mailing.news.warning", Durable: true},
+	},
+
+	RMQ_HEARTBEAT: {
+
+		Type:     RMQ,
+		Exchange: cr_rabbitmq.ExchangeInfo{Name: "heartbeat.direct", Kind: "direct", Durable: true},
+		Key:      cr_rabbitmq.BindingInfo{Key: "routing.heartbeat"},
 	},
 }
