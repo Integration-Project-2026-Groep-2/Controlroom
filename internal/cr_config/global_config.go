04dd4a2fc5f4349f7144932268a35ab7ad283bad
nasr <nsrddyn@gmail.com>
2026-05-22 10:49:08 +0200

    refactor(config): refactored the config stuff and threw the stuff into a struct. why i dont know i think its even less eficiant. but maybe it isnt. hmmm who knows? we should test this in the future

--- internal/cr_config/global_config.go
+++ internal/cr_config/global_config.go
@@ -3,20 +3,42 @@ package config
 import (
 	"fmt"
 	"os"
+	//	"strings"
 
 	"github.com/elastic/go-elasticsearch/v9"
 )
 
-var ElasticUrl string = os.Getenv("ELASTICSEARCH_URL")
+// NOTE(nasr): apparantly equal fold is case insentive
+
+// pfff see the logger im anoyed by this
+// var DebugMode bool = strings.EqualFold(os.Getenv("DebugMode"), "true")
+
+type kibanaConfig struct {
+	Url      string
+	UserName string
+	Password string
+}
+
+var KibanaConfig = kibanaConfig{
+	Url:      fmt.Sprintf("%s%s", map[bool]string{true: "http://localhost", false: os.Getenv("KIBANA_URL")}[os.Getenv("KIBANA_URL") == ""], map[bool]string{true: "", false: ":" + os.Getenv("PORT_KIBANA")}[os.Getenv("KIBANA_URL") == "" || os.Getenv("PORT_KIBANA") == ""]),
+	UserName: os.Getenv("KIBANA_USERNAME"),
+	Password: os.Getenv("KIBANA_PASSWORD"),
+}
 
 var ElasticConfig = elasticsearch.Config{
-	Addresses: []string{ElasticUrl},
+	Addresses: []string{os.Getenv("ELASTICSEARCH_URL")},
 	Username:  os.Getenv("CONTROLROOM_ES_USER"),
 	Password:  os.Getenv("CONTROLROOM_ES_PASS"),
 }
 
 // TODO(nasr): replace all of the services with a dynamic query to elastic that gathers the different service names
 // or we could do this while consuming the services. also interesting...
+
+// NOTE(nasr): hello this is future me talking about dynamic stuff. okay we are going to keep a list
+// of the default services. we're going to make use of the dynamic array trait of arrays in this lanague
+// and because increasing them isn't something that happens very often. we are going to do it.
+// so we keep a default list of the services, and we push stuff on to that array whenever we receive something new
+// if we stop receiving something we remove it. but for the intialization we should keep it i think
 var Services = [6]string{
 	"CRM",
 	"FACTURATIE",
@@ -28,10 +50,10 @@ var Services = [6]string{
 
 var Hostname string
 
-var KbnXsrfToken = "true"
+var LogsDashboardId = os.Getenv("DASHBOARD_LOGS_ID")
+var LogsDataViewId = os.Getenv("DATAVIEW_LOGS_ID")
 
-var KibanaURL = fmt.Sprintf("%s:%s", os.Getenv("http://localhost"), os.Getenv("PORT_KIBANA"))
-var DashboardID = os.Getenv("DASHBOARD_ID")
-var DataViewId = os.Getenv("DATAVIEW_ID")
-var KibanaUserName = os.Getenv("KIBANA_USERNAME")
-var KibanaPassword = os.Getenv("KIBANA_PASSWORD")
+var HeartbeatDashboardId = os.Getenv("DASHBOARD_HEARTBEATS_ID")
+var HeartbeatDataviewId = os.Getenv("DATAVIEW_HEARTBEATS_ID")
+
+var KbnXsrfToken = "true"
---
+++ pkg/logger/index.go
@@ -0,0 +1,58 @@
+package logger
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/elastic/go-elasticsearch/v9/esapi"
+	"github.com/mailru/easyjson"
+)
+
+// note(nasr): this is anoying because I liked to declare the debug mode in the globaal configuration but it was causing a 4 file import cylcle error ggrrrrrrr
+var debugMode = strings.EqualFold(os.Getenv("DebugMode"), "true")
+
+func IndexLogsQueue() {
+
+	for payload := range queue {
+
+		if debugMode {
+
+			log.Println(payload)
+
+		}
+
+		data, err := easyjson.Marshal(payload)
+		if err != nil {
+			fmt.Printf("marshal error: %v\n", err)
+			continue
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+		req := esapi.IndexRequest{
+			Index:   Index,
+			Body:    bytes.NewReader(data),
+			Refresh: "false",
+		}
+
+		res, err := req.Do(ctx, Client)
+		cancel()
+
+		if err != nil {
+			fmt.Printf("index logs error: %v\n", err)
+			continue
+		}
+
+		res.Body.Close()
+
+		if res.IsError() {
+			fmt.Printf("index logs error response: %v\n", res.Status())
+			continue
+		}
+	}
+}
