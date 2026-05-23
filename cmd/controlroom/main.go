844fd540f8a78095d44f2f1c9ac2b74588c43594
nasr <nsrddyn@gmail.com>
2026-05-23 22:18:34 +0200

    performance(concurrency-consumption): more go routines please go faster

--- cmd/controlroom/main.go
+++ cmd/controlroom/main.go
@@ -203,7 +203,16 @@ func startSession(ctx context.Context, client *elasticsearch.Client) error {
 
 		switch def.Type {
 		case config.HEARTBEAT:
+
 			go cr_rabbitmq.Consume(cfg, msgs, ctx, heartbeat.ProcessHeartbeat)
+			go cr_rabbitmq.Consume(cfg, msgs, ctx, heartbeat.ProcessHeartbeat)
+
+			go cr_rabbitmq.Consume(cfg, msgs, ctx, heartbeat.ProcessHeartbeat)
+			go cr_rabbitmq.Consume(cfg, msgs, ctx, heartbeat.ProcessHeartbeat)
+
+			go cr_rabbitmq.Consume(cfg, msgs, ctx, heartbeat.ProcessHeartbeat)
+			go cr_rabbitmq.Consume(cfg, msgs, ctx, heartbeat.ProcessHeartbeat)
+
 		case config.LOGGER:
 			go cr_rabbitmq.Consume(cfg, msgs, ctx, cr_logger.ProcessLog)
 		case config.STATUSCHECK:
